f10f62b813a39a6e7ccc62e27a6deca662621bef
Michael Perry <lmperry@stanford.edu>
2017-08-30 14:12:09 -0700

    remove helper scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Builds the container.
-# The container can be exported using the export.sh script
-CONTAINER=flywheel/mri-deface
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag $CONTAINER $DIR
